411a5c57733f5b83137384620a109991fbea4582
Vedika Udgir <vedikaudgir@gmail.com>
2025-07-01 16:10:18 +0530

    Add files via upload

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        //public ApplicationDBContext(DbContextOptions options) : base(options)
+        //{
+        //}
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+
+    }
+}
